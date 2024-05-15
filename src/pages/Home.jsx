1f6b6c943a0e39dfa6c18f3b1b8acd50c842ad87
Ritesh Arora <68393812+ritesh2909@users.noreply.github.com>
2024-05-15 17:12:06 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,13 @@ function Home() {
       <NavbarV2 />
       <div className="carousel" style={{ marginTop: "50px" }}>
         <Carousel />
-        <video controls>
+      </div>
+      <div  style={{ width: "70%" }}>
+               <video controls>
               <source src="https://s3.ap-south-1.amazonaws.com/testbucket3.0/VID_20230901_222523.mp4" type="video/mp4" />
               Your browser does not support the video tag.
               </video>    
+        
       </div>
       {/* <OffShore /> */}
       <ProductList />
